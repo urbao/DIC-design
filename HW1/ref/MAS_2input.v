afe0f2216a48750c3379aad45fb1ad8be44999e3
urbao <E24094198@gs.ncku.edu.tw>
2024-03-11 13:07:18 +0800

    Update HW1 Files

---
+++ HW1/ref/MAS_2input.v
@@ -0,0 +1,16 @@
+// 
+// Designer: <student ID> 
+//
+module MAS_2input(
+    input signed [4:0]Din1,
+    input signed [4:0]Din2,
+    input [1:0]Sel,
+    input signed[4:0]Q,
+    output [1:0]Tcmp,
+    output signed [4:0]TDout,
+    output signed [3:0]Dout
+);
+
+/*Write your design here*/
+
+endmodule
