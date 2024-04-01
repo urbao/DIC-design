9822a4ab8e261d2163e514bdf65f5b2348f04040
urbao <E24094198@gs.ncku.edu.tw>
2024-04-01 13:05:50 +0800

    Initial commit to HW2

---
+++ HW2/RTL/CIPU.v
@@ -0,0 +1,23 @@
+module CIPU(
+input       clk, 
+input       rst,
+input       [7:0]people_thing_in,
+input       ready_fifo,
+input       ready_lifo,
+input       [7:0]thing_in,
+input       [3:0]thing_num,
+output      valid_fifo,
+output      valid_lifo,
+output      valid_fifo2,
+output      [7:0]people_thing_out,
+output      [7:0]thing_out,
+output      done_thing,
+output      done_fifo,
+output      done_lifo,
+output      done_fifo2);
+
+assign done_fifo=ready_fifo;
+assign done_lifo=ready_lifo;
+assign done_fifo2=ready_lifo;
+
+endmodule
