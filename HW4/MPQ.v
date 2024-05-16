f02862f13741be3af3082313764d10c9e50cd3b5
urbao <E24094198@gs.ncku.edu.tw>
2024-05-16 23:35:27 +0800

    Update MPQ.v - init the vars and confirm the design idea

--- HW4/MPQ.v
+++ HW4/MPQ.v
@@ -9,8 +9,14 @@ input [7:0] index;
 input [7:0] value;
 output reg busy;
 output reg RAM_valid;
-output reg[7:0]RAM_A;
+output reg [7:0]RAM_A;
 output reg [7:0]RAM_D;
 output reg done;
+
+/*------------ Max-Priority Queue related varaibles -------------*/
+// Since the Queue of pseudo-code provided by TAs starts from index 1,
+// the Queue[0] is left unused, and  Queue[1] to Queue[255] are used
+reg [7:0] Queue[0:255]; // store input value
+reg [7:0] LENGTH; // length of Queue(Queue[0] is ignored), maximum to 255
 
 endmodule
